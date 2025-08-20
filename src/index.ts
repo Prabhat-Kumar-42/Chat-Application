d7f6b4f91be19afd6c55d37feaf0dbcd77bc2316
Prabhat <prabhatkumarverma42@gmail.com>
2025-08-20 18:12:58 +0530

    feat: add Express server setup with CORS, route protection, and socket server integration

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { authRouter } from "./routes/auth.route";
+import { usersRouter } from "./routes/users.route";
+import { convRouter } from "./routes/conversations.route";
+import { initSocket } from "../src/socket/socket";
+import { isAuthenticated} from "./middlewares/auth.middleware";
+
+const app = express();
+app.use(cors({ origin: process.env.CLIENT_ORIGIN?.split(",") || "*" }));
+app.use(express.json());
+
+app.use("/auth", authRouter);
+app.use("/users", isAuthenticated, usersRouter);
+app.use("/conversations", isAuthenticated, convRouter);
+
+const server = http.createServer(app);
+initSocket(server);
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`✅ Server running :${PORT}`));
