e5635ddda2e55f27181ea307392296da6cc8cc2f
Prabhat <prabhatkumarverma42@gmail.com>
2025-08-20 18:03:54 +0530

    feat: added middleware: "isAuthenticated" for token validation and user authentication

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyJwt } from "../util/jwt-auth.util";
+import { AuthRequest } from "../types/auth-request.type";
+
+export const isAuthenticated = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const hdr = req.headers.authorization;
+  if (!hdr) return res.status(401).json({ error: "Missing token" });
+  const token = hdr.replace("Bearer ", "");
+  try {
+    req.user = verifyJwt(token);
+    next();
+  } catch {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
